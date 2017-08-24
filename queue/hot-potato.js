81207956c10aded6a6dffd572ad9ed01dfe6d700
Jeremy Fan <jeremyfan_14@live.com>
2017-08-24 23:21:33 +0800

    循环队列

---
+++ queue/hot-potato.js
@@ -0,0 +1,34 @@
+/**
+ * @fileOverview 击鼓传花
+ * @description 
+ *   一个循环队列的实现
+ *   依赖 Queue.js
+ */
+
+/**
+ * [hotPotato description]
+ * @param  {[type]} nameList 玩家列表
+ * @param  {[type]} num      传递次数
+ * @return {[type]}          赢家
+ */
+function hotPotato(nameList, num) {
+  let queue = new Queue(nameList)
+
+  while (queue.size() > 1) {
+    for (i = 0; i < num; i++) {
+      // 传到下一玩家的过程是当前玩家从队首到队尾的过程，即先出队再入队
+      queue.enqueue(queue.dequeue())
+    }
+
+    const eliminated = queue.dequeue()
+    console.log(`${eliminated} was eliminated from the Hot Potato game.`)
+  }
+
+  return queue.dequeue()
+}
+
+
+// use
+const nameList = ['Jack', 'John', 'Jeremy', 'Eric', 'Chris']
+const winner = hotPotato(nameList, 7)
+console.log(winner) // Jack
