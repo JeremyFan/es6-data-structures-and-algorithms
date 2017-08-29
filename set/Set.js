a9ca6d1368ab287438c87c77d4dbe0cd2c9434f3
Jeremy Fan <jeremyfan_14@live.com>
2017-08-29 23:41:16 +0800

    set

---
+++ set/Set.js
@@ -0,0 +1,34 @@
+/**
+ * @fileOverview 集合
+ * @description ES6 Set
+ */
+
+class Set {
+  constructor() {
+    this._items = {}
+  }
+
+  has(value){
+
+  }
+
+  add(value){
+
+  }
+
+  remove(value){
+
+  }
+
+  clear(){
+
+  }
+
+  size(){
+
+  }
+
+  values(){
+    
+  }
+}
